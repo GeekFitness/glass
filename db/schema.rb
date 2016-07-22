cf05e79f99095ff9e12acaae3f876f29c763784d
BJ Keeton <bjk@geekfitness.net>
2016-07-22 13:37:27 -0500

    added password to Clients

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722172752) do
+ActiveRecord::Schema.define(version: 20160722180028) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_clients_on_email", unique: true
   end
 
   create_table "events", force: :cascade do |t|
