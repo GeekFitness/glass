e62b1285b3fd9ec2569c3a9c04ee8e74ddeae697
BJ Keeton <bjk@geekfitness.net>
2016-07-22 12:32:05 -0500

    fixed forms, client model validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722165240) do
+ActiveRecord::Schema.define(version: 20160722172752) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20160722165240) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.float    "duration"
     t.text     "address"
     t.string   "phone"
     t.text     "comments"
